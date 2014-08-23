13032c9a174edf11c5f1b8261c462a5051c7fad9
Daniel Lowes <daniel@wmgc.co.za>
2014-08-23 11:41:39 +0200

    Direct import of backbone.routefilter project

--- backbone.subroute.js
+++ backbone.subroute.js
@@ -1,6 +1,6 @@
 // backbone-subroute.js v0.4.3
 //
-// Copyright (C) 2012 Dave Cadwallader, Model N, Inc.  
+// Copyright (C) 2012 Dave Cadwallader, Model N, Inc.
 // Distributed under the MIT License
 //
 // Documentation and full license available at:
@@ -19,6 +19,8 @@
     }
 }(function(_, Backbone) {
 
+    var nop = function(){};
+
     Backbone.SubRoute = Backbone.Router.extend({
         constructor: function(prefix, options) {
 
@@ -30,7 +32,7 @@
 
             // SubRoute instances may be instantiated using a prefix with or without a trailing slash.
             // If the prefix does *not* have a trailing slash, we need to insert a slash as a separator
-            // between the prefix and the sub-route path for each route that we register with Backbone.        
+            // between the prefix and the sub-route path for each route that we register with Backbone.
             this.separator = (prefix.slice(-1) === "/") ? "" : "/";
 
             // if you want to match "books" and "books/" without creating separate routes, set this
@@ -48,7 +50,7 @@
                 hash = Backbone.history.getHash();
             }
 
-            // Trigger the subroute immediately.  this supports the case where 
+            // Trigger the subroute immediately.  this supports the case where
             // a user directly navigates to a URL with a subroute on the first page load.
             // Check every element, if one matches, break. Prevent multiple matches
             _.every(this.routes, function(key, route) {
@@ -74,8 +76,102 @@
             }
             Backbone.Router.prototype.navigate.call(this, route, options);
         },
+
+        // Pave over Backbone.Router.prototype.route, the public method used
+        // for adding routes to a router instance on the fly, and the
+        // method which backbone uses internally for binding routes to handlers
+        // on the Backbone.history singleton once it's instantiated.
         route: function(route, name, callback) {
-            // strip off any leading slashes in the sub-route path, 
+
+            //console.log("Routing", route, name, callback);
+          // If there is no callback present for this route, then set it to
+          // be the name that was set in the routes property of the constructor,
+          // or the name arguement of the route method invocation. This is what
+          // Backbone.Router.route already does. We need to do it again,
+          // because we are about to wrap the callback in a function that calls
+          // the before and after filters as well as the original callback that
+          // was passed in.
+          if( !callback ){
+            callback = this[ name ];
+          }
+
+          // Create a new callback to replace the original callback that calls
+          // the before and after filters as well as the original callback
+          // internally.
+          var wrappedCallback = function() {
+
+            // Call the before filter and if it returns false, run the
+            // route's original callback, and after filter. This allows
+            // the user to return false from within the before filter
+            // to prevent the original route callback and after
+            // filter from running.
+            var callbackArgs = [ route, _.toArray(arguments) ];
+            var beforeCallback;
+
+            if ( _.isFunction(this.before) ) {
+
+              // If the before filter is just a single function, then call
+              // it with the arguments.
+              beforeCallback = this.before;
+            } else if ( typeof this.before[route] !== "undefined" ) {
+
+              // otherwise, find the appropriate callback for the route name
+              // and call that.
+              beforeCallback = this.before[route];
+            } else {
+
+              // otherwise, if we have a hash of routes, but no before callback
+              // for this route, just use a nop function.
+              beforeCallback = nop;
+            }
+
+            // If the before callback fails during its execusion (by returning)
+            // false, then do not proceed with the route triggering.
+            if ( beforeCallback.apply(this, callbackArgs) === false ) {
+              return;
+            }
+
+            // If the callback exists, then call it. This means that the before
+            // and after filters will be called whether or not an actual
+            // callback function is supplied to handle a given route.
+            if( callback ) {
+              callback.apply( this, arguments );
+            }
+
+            var afterCallback;
+            if ( _.isFunction(this.after) ) {
+
+              // If the after filter is a single funciton, then call it with
+              // the proper arguments.
+              afterCallback = this.after;
+
+            } else if ( typeof this.after[route] !== "undefined" ) {
+
+              // otherwise if we have a hash of routes, call the appropriate
+              // callback based on the route name.
+              afterCallback = this.after[route];
+
+            } else {
+
+              // otherwise, if we have a has of routes but no after callback
+              // for this route, just use the nop function.
+              afterCallback = nop;
+            }
+
+            // Call the after filter.
+            afterCallback.apply( this, callbackArgs );
+
+          }.bind(this);
+
+          // Call our original route, replacing the callback that was originally
+          // passed in when Backbone.Router.route was invoked with our wrapped
+          // callback that calls the before and after callbacks as well as the
+          // original callback.
+          return this.subroute.call( this, route, name, wrappedCallback );
+        },
+
+        subroute: function(route, name, callback) {
+            // strip off any leading slashes in the sub-route path,
             // since we already handle inserting them when needed.
             if (route.substr(0) === "/") {
                 route = route.substr(1, route.length);
@@ -97,7 +193,7 @@
             // remove the un-prefixed route from our routes hash
             delete this.routes[route];
 
-            // add the prefixed-route.  note that this routes hash is just provided 
+            // add the prefixed-route.  note that this routes hash is just provided
             // for informational and debugging purposes and is not used by the actual routing code.
             this.routes[_route] = name;
 
